e10361d421c67e91d532d2febbc31772fbd6a309
Christopher Allène <cwiiss@gmail.com>
2024-06-09 08:32:15 +0200

    fix: progress bar and animation with 5.2

--- xdrip/View Controllers/Root View Controller/RootViewController.swift
+++ xdrip/View Controllers/Root View Controller/RootViewController.swift
@@ -2788,7 +2788,7 @@ final class RootViewController: UIViewController, ObservableObject {
             
             
             // disable the chart animation if it's just a normal update, enable it if the call comes from didAppear()
-            if animate {
+            if animate && UserDefaults.standard.animateProgressBar {
                 self.pieChartOutlet.animDuration = ConstantsStatistics.pieChartAnimationSpeed
             } else {
                 self.pieChartOutlet.animDuration = 0
@@ -3164,7 +3164,12 @@ final class RootViewController: UIViewController, ObservableObject {
                         } else {
 
                             // fill in the labels to show sensor time elapsed and max age
-                            dataSourceSensorCurrentAgeOutlet.text = sensorStartDate?.daysAndHoursAgo()
+                            if UserDefaults.standard.reverseProgressBar {
+                                dataSourceSensorCurrentAgeOutlet.text = sensorTimeLeftInMinutes.minutesToDaysAndHours()
+                            } else {
+                                dataSourceSensorCurrentAgeOutlet.text = sensorStartDate?.daysAndHoursAgo()
+                            }
+
 
                             dataSourceSensorMaxAgeOutlet.text = " / " + sensorMaxAgeInMinutes.minutesToDaysAndHours()
 
@@ -3174,7 +3179,11 @@ final class RootViewController: UIViewController, ObservableObject {
                 } else {
 
                     // fill in the labels to show sensor time elapsed and max age
-                    dataSourceSensorCurrentAgeOutlet.text = sensorStartDate?.daysAndHoursAgo()
+                    if UserDefaults.standard.reverseProgressBar {
+                        dataSourceSensorCurrentAgeOutlet.text = sensorTimeLeftInMinutes.minutesToDaysAndHours()
+                    } else {
+                        dataSourceSensorCurrentAgeOutlet.text = sensorStartDate?.daysAndHoursAgo()
+                    }
 
                     dataSourceSensorMaxAgeOutlet.text = " / " + sensorMaxAgeInMinutes.minutesToDaysAndHours()
 
